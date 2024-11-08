e4341e41d8af63044bafa8f733e37d51d285a0ee
Rodrigo <rjaencobos@safareyes.es>
2024-11-08 22:46:58 +0100

    Comienzo del header.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isUserMenuVisible = false;
 
+  toggleUserMenu() {
+    this.isUserMenuVisible = !this.isUserMenuVisible;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {DemoComponent} from '../demo/demo.component';
+import {HeaderComponent} from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    DemoComponent
+    DemoComponent,
+    HeaderComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'

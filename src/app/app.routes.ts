0b8124273607433165fea48e323143eeb48de108
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-18 13:38:13 +0100

    Habilitacion de ruta para el metdodo de validar proveedores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import {DemoComponent} from './demo/demo.component';
 import {ProductosProveedorComponent} from './productos-proveedor/productos-proveedor.component';
 import {OngDetallesComponent} from './ong-detalles/ong-detalles.component';
+import {ValidarProveedorComponent} from './validar-proveedor/validar-proveedor.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -16,5 +17,7 @@ export const routes: Routes = [
   {path:'ong/detalles', component:OngDetallesComponent},
   {path:'ong/detalles/:id', component:OngDetallesComponent},
   {path:'acontecimiento/ong/:id/acontecimientos', component:OngDetallesComponent},
+  {path:'ong/validar-proveedor', component:ValidarProveedorComponent},
+  {path:'ong/validar-proveedor/:id', component:ValidarProveedorComponent},
   { path: '**', redirectTo: '/login' } // Redirecciona rutas no encontradas al login
 ];

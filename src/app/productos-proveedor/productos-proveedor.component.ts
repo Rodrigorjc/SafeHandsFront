e9ff9182e00690fb68c1ee1061bd1fbab3c267a6
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-19 20:12:11 +0100

    implemenatcion de la busqueda de productos por proveedor

--- src/app/productos-proveedor/productos-proveedor.component.ts
+++ src/app/productos-proveedor/productos-proveedor.component.ts
@@ -3,7 +3,9 @@ import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ProductoService } from '../services/producto.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, NgForm, Validators} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
 
 interface Product {
   nombre: string;
@@ -18,7 +20,8 @@ interface Product {
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './productos-proveedor.component.html',
   styleUrl: './productos-proveedor.component.css'
@@ -27,30 +30,44 @@ export class ProductosProveedorComponent implements OnInit {
   products: Product[] = [];
   showForm: boolean = false;
   newProduct: Product = { nombre: '', url: '' , descripcion: '', precio: 0};
+    productForm: FormGroup;
+    productoId: string | null = null;
 
-  constructor(private productoService: ProductoService) {}
-
+  constructor(private productoService: ProductoService, private fb: FormBuilder, private route: ActivatedRoute) {
+    this.productForm = this.fb.group({
+      nombre: ['', Validators.required],
+      url: ['', Validators.required],
+      precio: [0, [Validators.required, Validators.min(0.01)]],
+      descripcion: ['', Validators.required]
+    });
+  }
   ngOnInit() {
-    this.productoService.obtenerProductos().subscribe({
-      next: (fetchedProducts) => {
-        this.products = fetchedProducts;
-      },
-      error: (err) => console.error('Error fetching products', err)
-    });
+    this.productoId = this.route.snapshot.paramMap.get('id');
+    if (this.productoId) {
+      this.productoService.obtenerProductoId(this.productoId).subscribe({
+        next: (data) => {
+          this.products = data;
+        },
+        error: (err) => console.error('Error fetching products', err)
+      });
+    }
   }
+    toggleForm(){
+      this.showForm = !this.showForm;
+    }
 
-  toggleForm() {
-    this.showForm = !this.showForm;
-  }
+    addProduct(){
+      if (this.productForm.invalid) {
+        return;
+      }
 
-  addProduct() {
-    this.productoService.crearProducto(this.newProduct).subscribe({
-      next: (createdProduct) => {
-        this.products.push(createdProduct);
-        this.newProduct = { nombre: '', url: '', descripcion: '', precio: 0 };
-        this.showForm = false;
-      },
-      error: (err) => console.error('Error creating product', err)
-    });
-  }
+      this.productoService.crearProducto(this.productForm.value).subscribe({
+        next: (createdProduct) => {
+          this.products.push(createdProduct);
+          this.productForm.reset();
+          this.showForm = false;
+        },
+        error: (err) => console.error('Error creating product', err)
+      });
+    }
 }

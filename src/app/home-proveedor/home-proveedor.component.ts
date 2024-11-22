87d86d62b8a4bcb0b597b52e99616a6b350b5e02
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-22 12:48:38 +0100

    se ha iniciado el home-proveedor

--- src/app/home-proveedor/home-proveedor.component.ts
+++ src/app/home-proveedor/home-proveedor.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-proveedor',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home-proveedor.component.html',
   styleUrl: './home-proveedor.component.css'
 })

98a164e92ad420d5f9a0d01bc9cd8691f418b063
Rodrigo <rjaencobos@safareyes.es>
2024-11-24 17:30:03 +0100

    Instalacion de cloudinary para subir un enlace en vez de una foto a nuestra base de datos. Prueba en componente demo.

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,16 +1,22 @@
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {AuthService} from '../services/auth.service';
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {UploadImgComponent} from '../upload-img/upload-img.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-demo',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    UploadImgComponent,
+    NgIf
+  ],
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.css'
 })
 export class DemoComponent {
-  constructor(private authService: AuthService, private http: HttpClient) {}
 
 
   onDemoButtonClick() {
@@ -27,5 +33,32 @@ export class DemoComponent {
       (data: any) => console.log('Remaining Time:', data),
       error => console.error('Error:', error)
     );
+  }
+
+  myForm: FormGroup;
+  imageUrl: string | null = null;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private http: HttpClient) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      // email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onImageUploaded(imageUrl: string) {
+    this.imageUrl = imageUrl;
+    console.log('URL de la imagen recibida:', imageUrl);
+  }
+
+  onSubmit() {
+    const formData = {
+      ...this.myForm.value,
+      imageUrl: this.imageUrl, // Incluir la URL de la imagen
+    };
+
+    console.log('Datos del formulario:', formData);
+
+    // Simula un mensaje de éxito
+    alert('Formulario enviado con éxito. Revisa la consola para ver los datos.');
   }
 }

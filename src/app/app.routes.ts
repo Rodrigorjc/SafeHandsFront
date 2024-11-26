2536e99f3caa31cdcf9c277b65f3307fb67f1875
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-26 20:48:42 +0100

    habilitacion de nueva ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ import {
 } from './vincular-acontecimiento-productos/vincular-acontecimiento-productos.component';
 import {OngAsociarAcontecimientoComponent} from './ong-asociar-acontecimiento/ong-asociar-acontecimiento.component';
 import {MovilidadProveedoresComponent} from './movilidad-proveedores/movilidad-proveedores.component';
+import {AdminOngComponent} from './admin-ong/admin-ong.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -35,6 +36,7 @@ export const routes: Routes = [
   {path:'proveedor/vincular/acontecimiento/:id', component:VincularAcontecimientoProductosComponent},
   {path:'ong/acontecimientos/:id', component:OngAsociarAcontecimientoComponent},//para ong
   {path:'movilidad-proveedores/:id', component:MovilidadProveedoresComponent},
+  {path:'admin/ong', component:AdminOngComponent},
   { path: '', redirectTo: 'movilidad-proveedores', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' } // Redirecciona rutas no encontradas al login
 

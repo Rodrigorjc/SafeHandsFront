15faf4d8f5acd72ed48ba557a205d29ea6231e78
Rodrigo <rjaencobos@safareyes.es>
2024-11-26 20:17:18 +0100

    Busqueda de acontecimineto por id añadida a su servicio.

--- src/app/services/acontecimiento.service.ts
+++ src/app/services/acontecimiento.service.ts
@@ -2,18 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import {Observable} from 'rxjs';
+import {Acontecimineto} from '../modelos/Acontecimineto';
 
 @Injectable({ providedIn: 'root' })
 export class AcontecimientoService {
-  private authUrl = 'http://localhost:8081/acontecimiento';
 
   constructor(private http: HttpClient) {}
 
   getAcontecimiento(): Observable<any[]> {
-    return this.http.get<any[]>(this.authUrl+"/listar");
+    return this.http.get<any[]>("/api/acontecimiento/listar");
   }
 
   getAcontecimientosByOngId(ongId: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.authUrl}/ong/${ongId}/acontecimientos`);
+    return this.http.get<any[]>(`/api/acontecimiento/ong/${ongId}/acontecimientos`);
+  }
+
+  getAcontecimientoById(id: number): Observable<Acontecimineto> {
+    return this.http.get<Acontecimineto>(`/api/acontecimiento/getById/${id}`)
   }
 }

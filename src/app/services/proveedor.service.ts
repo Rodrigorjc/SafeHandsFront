8f7c884c88498369c948862ef855aaa5296dba52
davidmato <dmatodelalozapalomo@safareyes.es>
2024-11-28 13:23:34 +0100

    cambio en la url ya que estaba mal

--- src/app/services/proveedor.service.ts
+++ src/app/services/proveedor.service.ts
@@ -16,7 +16,7 @@ export class ProveedorService {
     return this.http.get<any>(`/api/proveedor/listado`);
   }
   getProveedor(id: string): Observable<any> {
-    return this.http.get<any>(`/api/obtenerId/${id}`);
+    return this.http.get<any>(`/api/proveedor/obtenerId/${id}`);
   }
 
 }

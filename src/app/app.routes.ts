b645be259f988fa12b0cce9c9d77db17a4a2152f
Rodrigo <rjaencobos@safareyes.es>
2024-12-01 18:41:41 +0100

    Cambios en el routes para añadir detalles del carrito.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,6 +25,7 @@ import {ListAconteciminetosComponent} from './list-aconteciminetos/list-aconteci
 import {InfoAcontecimientoComponent} from './info-acontecimiento/info-acontecimiento.component';
 import {ListadoProductosComponent} from './listado-productos/listado-productos.component';
 import {ListProveedoresComponent} from './list-proveedores/list-proveedores.component';
+import {DetalleCarritoComponent} from './detalle-carrito/detalle-carrito.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -58,5 +59,6 @@ export const routes: Routes = [
   { path: 'informacion/acontecimiento/:id', component: InfoAcontecimientoComponent},
   { path: 'donar/:id', component: ListadoProductosComponent},
   { path: 'listado/proveedores', component: ListProveedoresComponent},
+  { path: 'carrito/detalles', component: DetalleCarritoComponent},
   { path: '**', redirectTo: '/home' } // Redirecciona rutas no encontradas al login
 ];

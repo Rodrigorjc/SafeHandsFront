58d1fcb17d254570f3eb6326665f3dff12b7903b
Rodrigo <rjaencobos@safareyes.es>
2024-12-02 19:16:06 +0100

    Cambios en la rutas para implementar /api/.

--- src/app/services/acontecimiento.service.ts
+++ src/app/services/acontecimiento.service.ts
@@ -28,16 +28,18 @@ export class AcontecimientoService {
     return this.http.get<Acontecimineto>(`/api/acontecimiento/getById/${id}`)
   }
 
-  getAcontecimientoById(acontecimientoId: string): Observable<any> {
-    return this.http.get<any>(`${this.authUrl}/detalles/${acontecimientoId}`);
+  getAcontecimientoByIdd(id: string): Observable<Acontecimineto> {
+    return this.http.get<Acontecimineto>(`/api/acontecimiento/getById/${id}`)
   }
 
+
+
   crearAcontecimientoOng(acontecimiento: any): Observable<any> {
-    return this.http.post<any>(`${this.authUrl}/crear/acontecimiento/admin/ong`, acontecimiento);
+    return this.http.post<any>(`/api/acontecimiento/crear/acontecimiento/admin/ong`, acontecimiento);
   }
 
   eliminarAcontecimieto(acontecimientoId: number): Observable<string> {
-    return this.http.delete(`${this.authUrl}/eliminar/${acontecimientoId}`,{ responseType:'text'} );
+    return this.http.delete(`/api/acontecimineto/eliminar/${acontecimientoId}`,{ responseType:'text'} );
   }
 
 }

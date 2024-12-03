ea398f48771d410f3b6bdc82f58eada7ac0d4218
Rodrigo <rjaencobos@safareyes.es>
2024-12-03 19:38:01 +0100

    Implementacion de la funcionalidad de añadir y eliminar productos del carrito en cantidad.

--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -3,6 +3,7 @@ import { Producto } from '../modelos/Producto';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {Pedido} from '../modelos/Pedido';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -65,5 +66,44 @@ export class CarritoService {
 
   pagarCarrito(pedido: Pedido): Observable<any>  {
     return this.http.post('/api/pedidos/realizar/pedido', pedido, { responseType: 'text' });
+  }
+
+  aumentarCantidad(producto: Producto) {
+    const productoExistente = this.productosEnCarrito.find(p => p.id === producto.id);
+    if (productoExistente) {
+      productoExistente.cantidad = (productoExistente.cantidad ?? 1) + 1;
+      if (productoExistente.precio !== undefined) {
+        productoExistente.total = productoExistente.cantidad * productoExistente.precio;
+      }
+      this.guardarCarrito();
+      this.carritoSubject.next(this.productosEnCarrito);
+    }
+  }
+
+  disminuirCantidad(producto: Producto) {
+    const productoExistente = this.productosEnCarrito.find(p => p.id === producto.id);
+    if (productoExistente && productoExistente.cantidad !== undefined) {
+      if (productoExistente.cantidad > 1) {
+        productoExistente.cantidad -= 1;
+        if (productoExistente.precio !== undefined) {
+          productoExistente.total = productoExistente.cantidad * productoExistente.precio;
+        }
+        this.guardarCarrito();
+        this.carritoSubject.next(this.productosEnCarrito);
+      } else {
+        Swal.fire({
+          title: '¿Desea borrar el producto?',
+          text: 'La cantidad es 1. Si continúa, el producto será eliminado del carrito.',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Sí, eliminar',
+          cancelButtonText: 'Cancelar'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.eliminarProducto(producto);
+          }
+        });
+      }
+    }
   }
 }

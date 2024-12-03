37f4e95d627e91e9e10aa2bdb895983b55da08ad
Rodrigo <rjaencobos@safareyes.es>
2024-12-03 11:10:33 +0100

    Cambio en la notificacion que se muestra cuando la sesion esta caducada.

--- src/app/interpector/interceptor.service.ts
+++ src/app/interpector/interceptor.service.ts
@@ -3,13 +3,15 @@ import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { catchError, Observable, throwError, tap } from 'rxjs';
+import {NotificacionService} from '../services/notificacion.service';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private notificacion: NotificacionService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.getToken(); // Assuming getToken() returns the token
@@ -38,6 +40,12 @@ export class InterceptorService implements HttpInterceptor {
           case 401:
             this.authService.logout();
             this.router.navigate(['/login']);
+            Swal.fire({
+              icon: 'warning',
+              title: '¡Sesión caducada!',
+              text: 'Tu sesión ha expirado. Por favor, inicia sesión nuevamente.',
+              confirmButtonText: 'Cerrar'
+            });
             break;
           case 403:
             console.log("Fallo");

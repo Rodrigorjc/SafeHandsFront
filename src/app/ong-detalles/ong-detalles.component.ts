2bde2ee8efdf6bbfb75393d66f8d6cd29f27f9ca
davidmato <dmatodelalozapalomo@safareyes.es>
2024-12-03 13:13:55 +0100

    se ha implementado  la funcion que saca el idOng cuando le pasas el idUsuario para que se pueda sacra el acontecimiento asociado a la ong

--- src/app/ong-detalles/ong-detalles.component.ts
+++ src/app/ong-detalles/ong-detalles.component.ts
@@ -1,15 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {HeaderComponent} from '../header/header.component';
 import {AcontecimientoService} from '../services/acontecimiento.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, RouterLink} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {OngService} from '../services/ong.service';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-ong-detalles',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule, ReactiveFormsModule, RouterLink
   ],
   templateUrl: './ong-detalles.component.html',
   styleUrl: './ong-detalles.component.css'
@@ -17,7 +18,10 @@ import {OngService} from '../services/ong.service';
 export class OngDetallesComponent implements OnInit {
   acontecimientos: any[] = [];
   ong: any = {};
-  ongId: string | null = null;
+  asociados: any[] = [];
+  noAsociados: any[] = [];
+  userId: any | null = null;
+  ongId: any | null = null;
 
   constructor(
     private route: ActivatedRoute,
@@ -26,27 +30,39 @@ export class OngDetallesComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.ongId = this.route.snapshot.paramMap.get('id');
-    if (this.ongId) {
-      this.ongService.getOngById(this.ongId).subscribe({
-        next: (data) => {
-          this.ong = data;
-        },
-        error: (err) => {
-          console.error('Error fetching ONG details', err);
-          alert(`Error fetching ONG details: ${err.message}`);
-        }
-      });
-      this.acontecimientoService.getAcontecimientosByOngId(this.ongId).subscribe({
-        next: (data) => {
-          this.acontecimientos = data;
-        },
-        error: (err) => {
-          console.error('Error fetching acontecimientos', err);
-          alert(`Error fetching acontecimientos: ${err.message}`);
-        }
-      });
+    this.userId = this.route.snapshot.paramMap.get('id');
+    if (this.userId) {
+      this.ongService.getIdOngPorIdUsuario(this.userId).subscribe({
+        next: (ongId) => {
+          this.ongId = ongId;
+          this.acontecimientoService.getAcontecimientosByOngId(this.ongId).subscribe({
+            next: (data) => {
+              this.asociados = data;
+              console.log('Acontecimientos:', data);
+              console.log("ONG ID:", this.ongId);
+            },
+            error: (err) => {
+              console.error('Error fetching acontecimientos', err);
+              alert(`Error fetching acontecimientos: ${err.message}`);
+            }
+          });
 
+          this.ongService.getOngById(this.userId).subscribe({
+            next: (data) => {
+              this.ong = data;
+              console.log('ONG:', data);
+            },
+            error: (err) => {
+              console.error('Error fetching ONG details', err);
+              alert(`Error fetching ONG details: ${err.message}`);
+            }
+          });
+        },
+        error: (err) => {
+          console.error('Error fetching ONG ID', err);
+          alert(`Error fetching ONG ID: ${err.message}`);
+        }
+      });
     }
   }
 }

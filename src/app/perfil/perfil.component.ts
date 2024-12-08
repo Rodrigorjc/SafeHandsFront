3e54940b4eb363b91c4744b5e800ffb1128a5db4
Rodrigo <rjaencobos@safareyes.es>
2024-12-08 13:20:00 +0100

    Metodo para el listado de aconteciminetos y listado de productos donados en el perfil.component.ts.

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {NgIf} from '@angular/common';
-import {AuthService} from '../services/auth.service';
-import {Cliente} from '../modelos/Cliente';
-import {UploadImgComponent} from '../upload-img/upload-img.component';
-import {FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
+// src/app/perfil/perfil.component.ts
+import { Component, OnInit } from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import { AuthService } from '../services/auth.service';
+import { Cliente } from '../modelos/Cliente';
+import { UploadImgComponent } from '../upload-img/upload-img.component';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { NombreImg } from '../modelos/NombreImg';
 
 @Component({
   selector: 'app-perfil',
@@ -12,26 +14,29 @@ import Swal from 'sweetalert2';
     NgIf,
     UploadImgComponent,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgForOf
   ],
   templateUrl: './perfil.component.html',
   standalone: true,
   styleUrl: './perfil.component.css'
 })
-export class PerfilComponent implements OnInit{
-
+export class PerfilComponent implements OnInit {
   rol: string | null = '';
   userID: string | null = localStorage.getItem('userId');
-  numberId: number = Number(this.userID)
+  numberId: number = Number(this.userID);
   cliente: Cliente | null = null;
   perfilForm!: FormGroup;
+  productosDonados: NombreImg[] = [];
+  acontecimientosDonados: NombreImg[] = [];
 
-  constructor(private service: AuthService, private fb: FormBuilder) {
-  }
+  constructor(private service: AuthService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.getRol();
     this.getCliente(this.numberId);
+    this.getProductosDonados(this.numberId);
+    this.getAcontecimientosDonados(this.numberId);
     this.perfilForm = this.fb.group({
       dni: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -92,10 +97,31 @@ export class PerfilComponent implements OnInit{
     });
   }
 
+  getProductosDonados(userId: number) {
+    this.service.getProductosDonados(userId).subscribe({
+      next: (data) => {
+        this.productosDonados = data;
+      },
+      error: (err) => {
+        console.error('Error fetching productos donados', err);
+      }
+    });
+  }
+
+  getAcontecimientosDonados(userId: number) {
+    this.service.getAcontecimientosDonados(userId).subscribe({
+      next: (data) => {
+        this.acontecimientosDonados = data;
+      },
+      error: (err) => {
+        console.error('Error fetching acontecimientos donados', err);
+      }
+    });
+  }
+
   onImageUploaded(imageUrl: string) {
     if (this.cliente) {
       this.cliente.img = imageUrl;
     }
   }
-
 }

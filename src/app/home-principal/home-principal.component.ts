b9b36ce18931dbedc254602402f3b644be36c272
Rodrigo <rjaencobos@safareyes.es>
2024-12-09 12:01:17 +0100

    Se ha creado el componente de homePrincipal para vista principal para el usuario sin logguear.

---
+++ src/app/home-principal/home-principal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-principal',
+  imports: [],
+  templateUrl: './home-principal.component.html',
+  standalone: true,
+  styleUrl: './home-principal.component.css'
+})
+export class HomePrincipalComponent {
+
+}

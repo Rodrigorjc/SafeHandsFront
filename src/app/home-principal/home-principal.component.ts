bbff4f377759afccfdb952dbbe0561fbf050dc77
Rodrigo <rjaencobos@safareyes.es>
2024-12-09 12:36:58 +0100

    Logica para el slider principal de home-principal.component.ts.

--- src/app/home-principal/home-principal.component.ts
+++ src/app/home-principal/home-principal.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NgClass, NgForOf, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-home-principal',
-  imports: [],
+  imports: [
+    NgClass,
+    NgStyle,
+    NgForOf
+  ],
   templateUrl: './home-principal.component.html',
   standalone: true,
   styleUrl: './home-principal.component.css'
 })
-export class HomePrincipalComponent {
+export class HomePrincipalComponent implements OnInit, OnDestroy{
+  currentSlide = 0;
+  slides = [1, 2, 3];
+  intervalId: any;
 
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide > 0) ? this.currentSlide - 1 : this.slides.length - 1;
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide < this.slides.length - 1) ? this.currentSlide + 1 : 0;
+  }
+
+  goToSlide(index: number) {
+    this.currentSlide = index;
+  }
 }
